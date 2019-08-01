712ff2f1bcdab99a535da29d984e9f6eef501528
muslum <mcoban86@gmail.com>
2019-08-01 17:24:01 +0300

    header responsive is ok

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -4,7 +4,10 @@
       <a href="/" class="home">
         <img src="../assets/img/logo.png" alt="Logo" height="35">
       </a>
-      <nav class="nav">
+      <a @click="mobileMenuClicked = !mobileMenuClicked" :class="['mobile-menu-button', { clicked: mobileMenuClicked } ]">
+        <img src="../assets/img/menu.svg" alt="Mobile Menu" height="24" />
+      </a>
+      <nav :class="['nav', { visible: mobileMenuClicked }]">
         <ul>
           <li><a href="#">Home</a></li>
           <li><a href="#">About</a></li>
@@ -17,8 +20,20 @@
   </div>
 </template>
 
+<script>
+export default {
+  name: 'Header',
+  data () {
+    return {
+      mobileMenuClicked: false
+    }
+  }
+}
+</script>
+
 
 <style lang="scss">
+@import "../assets/styles/layouts";
 #header {
   width: 100%;
   background: #fff;
@@ -35,6 +50,14 @@
       display: block;
     }
   }
+
+  .mobile-menu-button {
+    position: absolute;
+    right: 0;
+    padding: 5px 15px;
+    display: none;
+    transition: ease all .2s;
+  }
   
   .nav {
     margin-left: auto;
@@ -48,6 +71,50 @@
         a {
           &:hover {
             color: #007BEB;
+          }
+        }
+      }
+    }
+  }
+
+  @media screen and (max-width: $mobile) {
+    padding: 0;
+    .home img {
+      padding: 10px 0;
+      height: 30px;
+    }
+    .mobile-menu-button {
+      display: flex;
+      align-items: center;
+      &.clicked {
+        transform: rotate(-90deg);
+      }
+    }
+    .nav {
+      display: none;
+      position: fixed;
+      left: 0;
+      top: 50px;
+      background: #fff;
+      z-index: 1;
+      width: 100%;
+      padding: 30px 0;
+      box-sizing: border-box;
+      height: calc(100% - 50px);
+      border-top: 1px solid #ddd;
+      &.visible {
+        display: block;
+      }
+      
+      ul {
+        li {
+          display: block;
+          box-sizing: border-box;
+          margin: 0;
+          a {
+            display: block;
+            padding: 15px 30px;
+            font-size: 21px;
           }
         }
       }
